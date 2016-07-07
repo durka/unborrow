354c01f13cb5fb49118303e92040cd35b0d4838c
David Avedissian <git@davedissian.com>
2016-07-07 15:26:04 +0100

    Updated to work with nested objects

--- src/lib.rs
+++ src/lib.rs
@@ -33,14 +33,14 @@ macro_rules! unborrow {
     // This rule fires when we have parsed all the arguments.
     // It just falls through to output stage.
     // (FIXME could fold the output rule into this one to reduce recursion)
-    (@parse () -> ($names:tt $lets:tt) $($thru:tt)*) => {
-        unborrow!(@out $names $lets $($thru)*)
+    (@parse () -> ($names:tt $lets:tt) $($obj:ident).+) => {
+        unborrow!(@out $names $lets $($obj).+)
     };
 
     // Parse an argument and continue parsing
     // This is the key rule, assigning a name for the argument and generating the let statement.
-    (@parse ($arg:expr, $($rest:tt)*) -> ([$($names:ident),*] [$($lets:stmt);*]) $($thru:tt)*) => {
-        unborrow!(@parse ($($rest)*) -> ([$($names,)* arg] [$($lets;)* let arg = $arg]) $($thru)*)
+    (@parse ($arg:expr, $($rest:tt)*) -> ([$($names:ident),*] [$($lets:stmt);*]) $($obj:ident).+) => {
+        unborrow!(@parse ($($rest)*) -> ([$($names,)* arg] [$($lets;)* let arg = $arg]) $($obj).+)
         //                                            ^                    ^
         // Right here an ident is created out of thin air using hygiene.
         // Every time the macro recurses, we get a new syntax context, so "arg" is actually a new identifier!
@@ -49,17 +49,17 @@ macro_rules! unborrow {
     // Output stage.
     // Assembles the let statements and variable names into a block which computes the arguments,
     // calls the method, and returns its result.
-    (@out [$($names:ident),*] [$($lets:stmt);*] $obj:ident $meth:ident) => {{
+    (@out [$($names:ident),*] [$($lets:stmt);*] $($obj:ident).+) => {{
         $($lets;)*
-        $obj.$meth($($names),*)
+        $($obj).+($($names),*)
     }};
 
     // =========================================================================================================
     // PUBLIC RULES
 
     // Macro entry point.
-    ($obj:ident . $meth:ident ($($args:expr),*)) => {
-        unborrow!(@parse ($($args,)*) -> ([] []) $obj $meth)
+    ($($obj:ident).+ ($($args:expr),*)) => {
+        unborrow!(@parse ($($args,)*) -> ([] []) $($obj).+)
         //                |               |  |   ^ info about the method call, saved for later
         //                |               |  ^ generated let statements
         //                |               ^ generated argument names
